d55cae2c5160eab5377b9d482d994e1214bf84d7
michal.zielinski <michal.zielinski@notoria.pl>
2025-09-15 12:47:05 +0200

    add basic routes (Accueil, Covoiturages, Dashboard)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import Covoiturages from "./pages/Covoiturages";
-// import Dashboard from "./pages/Dashboard";
-// import Login from "./pages/Login";
+import Covoiturages from "./pages/Covoiturages";
+import VueDetaille from "./components/Covoiturages/VueDetaille";
+import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Acceuil from "./pages/Acceuil";
 
@@ -10,9 +11,10 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Acceuil />} />
-        {/* <Route path="/covoiturages" element={<Covoiturages />} />
+        <Route path="/covoiturages" element={<Covoiturages />} />
+        <Route path="/covoiturage/:id" element={<VueDetaille />} />
         <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/login" element={<Login />} /> */}
+        <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
       </Routes>
     </Router>
